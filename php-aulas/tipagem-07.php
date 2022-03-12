ce67d94350c6c452fd5e00879d696fea7020b121
johnywalker696 <johnywalker696@gmail.com>
2022-03-12 12:49:09 -0300

    updata

---
+++ php-aulas/tipagem-07.php
@@ -0,0 +1,16 @@
+<?php
+print '<p>1º  A linguagem PHP , define o tipo com base no seu conteudo. Sendo assim tipada.</p>';
+
+print ' ';
+
+$codigo = '5';
+$nome = 'teste';
+
+var_dump ($codigo);
+var_dump ($nome);
+
+
+print '<p>2º Concatenação.</p>';
+print ' ';
+
+var_dump('nome ' . 'sobrenome');
